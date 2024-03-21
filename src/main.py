2f031fc99aced041550ef8d95a231a196f2e8c30
Roman Kolganov <rakolganov@mai.education>
2024-03-21 18:31:23 +0300

    upgrade student + find fix + more

--- src/main.py
+++ src/main.py
@@ -12,11 +12,9 @@ firebase = pyrebase.initialize_app(firebaseConfig)
 db = firebase.database()
 auth = firebase.auth()
 
-
-
 class DB:
     @staticmethod
-    def getHash(*args):
+    def _getHash(*args):
         hash = shake_256()
         for arg in args:
             hash.update(str(arg).encode())
@@ -24,16 +22,16 @@ class DB:
     
     @staticmethod
     def getHashByStudent(student: Student):
-        return DB.getHash(student.surname, student.name, student.age, student.phoneNumber)
+        return DB._getHash(student.surname, student.name, student.age, student.phoneNumber)
     
     @staticmethod
     def writeStudent(student):
-        hash = DB.getHash(student.surname, student.name, student.age, student.phoneNumber)
+        hash = DB.getHashByStudent(student)
         db.child("students").child(hash).set(student.__dict__())
         return hash
 
     @staticmethod
-    def getStudent(hash):
+    def getStudentByHash(hash):
         student = db.child("students").child(hash).get().val()
         if student is None:
             raise ValueError("Student not found")
@@ -42,34 +40,46 @@ class DB:
     @staticmethod
     def updateStudentByStudent(student: Student, changes: dict):
         # Получаем текущие данные о студенте
-        old_hash = DB.getHash(student.surname, student.name, student.age, student.phoneNumber)
-        current_data = DB.getStudent(old_hash).__dict__()
-
-        # Обновляем данные
-        for key, value in changes.items():
-            current_data[key] = value
+        old_hash = DB._getHash(student.surname, student.name, student.age, student.phoneNumber)
+        current_data = DB.getStudentByHash(old_hash)
         
         # Генерируем новый хеш
-        new_hash = DB.getHash(current_data['surname'], current_data['name'], current_data['age'], current_data['phoneNumber'])
+        new_hash = DB._getHash(current_data.surname, current_data.name, current_data.age, current_data.phoneNumber)
+
+        current_data = current_data.__dict__()
+        # Обновляем данные
+        newData = DB._updateValues(current_data, changes)
+
+        print(changes, current_data, sep="\n\n")
 
         # Создаем новую запись в базе данных
-        db.child("students").child(new_hash).set(current_data)
+        db.child("students").child(new_hash).update(newData)
         return new_hash
+    
+
+    def _updateValues(current_data: dict, changes: dict):
+        for key, value in changes.items():
+            if type(value) is dict:
+                for k, v in value.items():
+                    current_data[key][k] = v
+            else:
+                current_data[key] = value
+        return current_data
     
     @staticmethod
     def updateStudent(hash: str, changes: dict):
+    # ! Добавить проыверку на правильность ключей
         # Получаем текущие данные о студенте
-        current_data = DB.getStudent(hash).__dict__()
+        current_data = DB.getStudentByHash(hash).__dict__()
 
         # Обновляем данные
-        for key, value in changes.items():
-            current_data[key] = value
+        newData = DB._updateValues(current_data, changes)
         
         # Генерируем новый хеш
-        new_hash = DB.getHash(current_data)
+        new_hash = DB._getHash(current_data)
 
         # Создаем новую запись в базе данных
-        db.child("students").child(new_hash).set(current_data)
+        db.child("students").child(new_hash).update(newData)
 
         # Удаляем старую запись
         db.child("students").child(hash).remove()
@@ -84,17 +94,28 @@ class DB:
         # Получаем список студентов подходящих под все критерии одновременно
         students = set()
         for key, value in criterias.items():
-            result = db.child("students").order_by_child(key).equal_to(value).get()
+            # получаем путь до нужного критерия
+            path = Student.getPathToField(key)
+
+            if len(path) == 0:
+                raise ValueError(f"Invalid criteria {key}")
+            
+            result = db.child("students").order_by_child(path).equal_to(value).get()
+            
             for student in result.each():
                 students.add(Student(**student.val()))
 
-        return students
+        return list(students) if len(students) > 0 else None
         
 
 def main():
-    moscowStudents = DB.findByCriteria({"age": 19, "homeCity": "Москва"})
-    print(*moscowStudents, sep="\n")
+    student = Student(personal_info={'name': 'Николай', 'surname': 'Петров', 'age': 18, 'homeCity': 'Москва'}, 
+                      contact_info={'address': 'ул Ленина, 1', 'phoneNumber': 1234567890}, 
+                      study_info={'group': 'МИСИС', 'course': 1, 'pass_num': 1234, 'available_rooms': [1, 2, 3], 'ID': 1234567890, 'gradebookID': 1234567890})
+    hash = DB.writeStudent(student)
+    # print(*DB.findByCriteria({"homeCity": "Москва", "course": 2}))
 
+    DB.updateStudent(hash, {'study_info': {'available_rooms': [1, 2, 3, 4]}})
 
 # def get_emeil_and_password(confirmation=False):
 #     email = input("Email: ")
--- src/student.py
+++ src/student.py
@@ -1,31 +1,80 @@
 class Student():
-    def __init__(self,surname,name, age, homeCity, address, phoneNumber, group, cource, patronymic=None):
-        # ["surname", "name", "patronymic", "age", "homeCity", "address", "phoneNumber", "group", "cource"]
-        self.surname = surname
-        self.name = name
-        self.age = age
-        self.homeCity = homeCity
-        self.address = address
-        self.phoneNumber = phoneNumber
-        self.group = group
-        self.cource = cource
-        self.patronymic = patronymic
+    surname = None
+    name = None
+    patronymic = None
+    age = None
+    homeCity = None
+    address = None
+    phoneNumber = None
+    group = None
+    course = None
+    pass_num = None
+    available_rooms = None
+    ID = None
+    gradebookID = None
+
+    def __init__(self, personal_info: dict, contact_info: dict, study_info: dict):
+        try:
+            self.surname = personal_info['surname']
+            self.name = personal_info['name']
+            self.phoneNumber = contact_info['phoneNumber']
+            self.age = personal_info['age']
+        except KeyError:
+            raise ValueError("Not enough information")
+        
+        self.patronymic = personal_info.get('patronymic')
+        self.homeCity = personal_info.get('homeCity')
+        self.address = contact_info.get('address')
+        self.group = study_info.get('group')
+        self.course = study_info.get('course')
+        self.pass_num = study_info.get('pass_num')
+        self.available_rooms = study_info.get('available_rooms')
+        self.ID = study_info.get('ID')
+        self.gradebookID = study_info.get('gradebookID')
 
     def __str__(self):
-        return ', '.join([str(value) for key, value in self.__dict__().items() if value is not None])
-
+        return ', '.join([str(value) for _, value in self.__dict__().items() if value is not None])
+    
     def __dict__(self):
         return {
-            "surname": self.surname,
-            "name": self.name,
-            "patronymic": self.patronymic,
-            "age": self.age,
-            "homeCity": self.homeCity,
-            "address": self.address,
-            "phoneNumber": self.phoneNumber,
-            "group": self.group,
-            "cource": self.cource
+            "personal_info": {
+                "surname": self.surname,
+                "name": self.name,
+                "patronymic": self.patronymic,
+                "age": self.age,
+                "homeCity": self.homeCity,
+            },
+            "contact_info": {
+                "address": self.address,
+                "phoneNumber": self.phoneNumber,
+            },
+            "study_info": {
+                "group": self.group,
+                "course": self.course,
+                "pass_num": self.pass_num,
+                "available_rooms": self.available_rooms,
+                "ID": self.ID,
+                "gradebookID": self.gradebookID
+            }
         }
+    
+    @classmethod
+    def getPathToField(cls, fieldName: str):
+        # Получаем путь к полю в словаре студента
+        d = cls.__dict__['__dict__'](cls)
+        path = ""
+        for key, value in d.items():
+            if type(value) is dict:
+                for k, v in value.items():
+                    if k == fieldName:
+                        path += f'{key}/{k}'
+                        return path
+            elif key == fieldName:
+                path += f'{key}'
+                return path
+        
+        raise ValueError(f"Invalid field name '{fieldName}'")
+            
     
     # метод для создания множества (проверка на идентичность)
     def __eq__(self, other):
@@ -33,4 +82,4 @@ class Student():
     
     # метод для создания хэша
     def __hash__(self):
-        return hash(frozenset(self.__dict__().items()))
+        return hash(self.__str__())
