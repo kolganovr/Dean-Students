0dd0362093c940a230ec07bcbab7a7e9964650e2
Roman Kolganov <kolganov2003@gmail.com>
2024-05-23 13:51:20 +0300

    raise error if not autorized

--- src/firebase.py
+++ src/firebase.py
@@ -1,5 +1,8 @@
 import pyrebase
-from cfg import firebaseConfig
+try:
+    from cfg import firebaseConfig
+except ImportError:
+    raise Exception("Файл cfg.py необходимый для работы приложения не найден!")
 
 firebase = pyrebase.initialize_app(firebaseConfig)
 
